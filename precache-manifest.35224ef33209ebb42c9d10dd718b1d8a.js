08ae644e7a55715bad05c7d7f208adfdd2c03b85
Lokeswara Sane <lokeswara.sane@ggktech.com>
2020-01-29 22:26:51 +0530

    Initial dist subtree commit

---
+++ precache-manifest.35224ef33209ebb42c9d10dd718b1d8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6eb95d2e8be00abf507507fa9554dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be6ddd6e99d22697d8a",
+    "url": "/static/css/main.669d7f5d.chunk.css"
+  },
+  {
+    "revision": "6d761685890855992aed",
+    "url": "/static/js/2.36b1f0f1.chunk.js"
+  },
+  {
+    "revision": "a1d48210be369c3dd955921bed49708c",
+    "url": "/static/js/2.36b1f0f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3be6ddd6e99d22697d8a",
+    "url": "/static/js/main.fcb4cf69.chunk.js"
+  },
+  {
+    "revision": "de0e17107c2cfa452be7",
+    "url": "/static/js/runtime-main.7c54635c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
