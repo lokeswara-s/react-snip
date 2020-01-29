90af88a45c80fd6dc379c6064a57c42843d0f2de
lokeswara-sane <lokeswara-sane@lokeswara-sanes-iMac.local>
2020-01-30 01:19:18 +0530

    Layout

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -3,8 +3,34 @@ import { Row, Column } from "simple-flexbox";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import ObjectProperties from "./ObjectProperties";
+import OptionList from "./OptionList";
 
 class Root extends Component {
+
+
+  onDrop=(ev)=>{
+    ev.preventDefault();
+    console.log(ev.dataTransfer.getData("text/html"));
+    var data=ev.dataTransfer.getData("text/html");
+    var nodeCopy = document.getElementById(this.state.currentSelected).cloneNode(true);
+    nodeCopy.draggable = false
+  
+    nodeCopy.id = Math.random(); /* We cannot use the same ID */
+    nodeCopy.addEventListener("dragover",(e)=>{
+      e.target.style.border = "1px dashed black"
+    })
+    nodeCopy.addEventListener("dragleave",(e)=>{
+      e.target.style.border = "1px solid black"
+    })
+    nodeCopy.addEventListener("drop",(e)=>{
+      e.target.style.border = "unset"
+    })
+    ev.target.appendChild(nodeCopy);
+  }
+
+  onDragOver = (event) => {
+    event.preventDefault();
+}
   render() {
     return (
       <React.Fragment>
@@ -24,14 +50,18 @@ class Root extends Component {
               zIndex: 1
             }}
           >
-            
+             <OptionList  setCurrent={(e)=>{
+              this.setState({
+                currentSelected:  e
+              })
+            }}/>
           </Column>
           <Column
             flex={3}
             style={{ backgroundColor: "white",margin:16,  zIndex: 0, paddingTop: 32 }}
           >
         
-            <div id="root-component-container">
+            <div id="root-component-container" onDrop ={this.onDrop} onDragOver={this.onDragOver} > 
             
             </div>
           </Column>
